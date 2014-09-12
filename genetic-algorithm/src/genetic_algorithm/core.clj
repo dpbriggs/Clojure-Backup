dc2b9f40df9468b26f421366b3350b10f68e14ee
David <david.briggs1997@gmail.com>
2014-09-11 21:13:22 -0400

    Added fitness function

--- genetic-algorithm/src/genetic_algorithm/core.clj
+++ genetic-algorithm/src/genetic_algorithm/core.clj
@@ -6,6 +6,10 @@
 ;; Ascii map for city name gen
 (def ascii-map #(get (vec (map char (range 65 91))) %))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Generate city information  ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defn gen-city-name
   "Adds ascii char to number to identify city"
   [num]
@@ -18,18 +22,23 @@
   "Generates num-cities amount of random cities"
   [num-cities dist-x dist-y]
   ;; Create map of randomly generated cities
-
   (vec (loop [num num-cities
-               cities []]
-          (if (= num 0)
-            cities
-            (let [n-num (- num 1)
-                  new-city (struct city
-                                   (gen-city-name n-num)
-                                   (rand-int dist-x)
-                                   (rand-int dist-y))
-                  n-cities (conj cities new-city)]
-              (recur n-num n-cities))))))
+              cities []]
+         (if (= num 0)
+           cities
+           (let [n-num (- num 1)
+                 new-city (struct city
+                                  (gen-city-name n-num)
+                                  (rand-int dist-x)
+                                  (rand-int dist-y))
+                 n-cities (conj cities new-city)]
+             (recur n-num n-cities))))))
+
+(defn scramble-city-order
+  "Returns num amount of randomly permuted city routes"
+  [num cities]
+  (into [] (repeatedly num #(shuffle cities))))
+
 (defn distance-between
   "Finds distance between two cities"
   [city-1 city-2]
@@ -40,26 +49,34 @@
         ]
     (Math/pow (+ (* xdist xdist) (* ydist ydist)) 0.5)))
 
-(defn scramble-city-order
-  "Returns num amount of randomly permuted city routes"
-  [num cities]
-  (into [] (repeatedly num #(shuffle cities))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Start of fitness-test ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Routes are a vector of cities in which index[0] is first stop, etc
 
 (defn route-distance
+  "Finds total distance taken in route"
   [route]
-  (loop [ahead route
-         behind (rest route)
+  (loop [current-city route
+         next-city (rest route) ;; ensure
          distance 0]
-    (if (= (count behind) 0)
+    (if (= (count next-city) 0)
       {:distance distance :route route}
-      (let [n-ahead (rest ahead)
-            n-behind (rest behind)
+      ;; n --> new
+      (let [n-current-city (rest current-city)
+            n-next-city (rest next-city)
             n-distance (+ distance (distance-between
-                                   (first ahead)
-                                   (first behind)))]
-        (recur n-ahead n-behind n-distance)))))
+                                   (first current-city)
+                                   (first next-city)))]
+        (recur n-current-city n-next-city n-distance)))))
+
+(defn fitness-test
+  "Grabs top N routes"
+  [N routes]
+  (take N (sort-by :distance routes)))
+
+
 
 (defn -main
   "I don't do a whole lot ... yet."
